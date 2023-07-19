eb22576ea5bdc51783956977c0d595d59a5a1ab5
mvk1-nist <mvk1@nist.gov>
2023-07-19 17:18:32 -0400

    Slightly modified timecmp.sh for easier modification later

--- timecmp.sh
+++ timecmp.sh
@@ -3,6 +3,6 @@
 f=${1:-bods/unit-sphere.bod}
 e=${2:-0}
 printf "zeno-original time:"
-time ../zeno-original/zeno-build/zeno -i $f --num-walks=1000000 --num-interior-samples=100000 --csv-output-file /dev/null > /dev/null
+time ~/zeno-original/zeno-build/zeno -i $f --num-walks=1000000 --num-interior-samples=100000 --csv-output-file /dev/null > /dev/null
 printf "\nzeno-run-modified time:"
-time ../zeno-run-modified/zeno-build/zeno -i $f --num-walks=1000000 --num-interior-samples=100000 --csv-output-file /dev/null --expansion=$e > /dev/null
+time ~/zeno-run-modified/zeno-build/zeno -i $f --num-walks=1000000 --num-interior-samples=100000 --csv-output-file /dev/null --expansion=$e > /dev/null
