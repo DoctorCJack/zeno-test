c413af00cbf2895df0d02cd37c16a52b4a313851
mvk1-nist <mvk1@nist.gov>
2023-07-19 16:50:36 -0400

    Fixed comment in all-expansions.sh

--- all-expansions.sh
+++ all-expansions.sh
@@ -5,7 +5,7 @@ ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 
 f=$1
-# There are currently options 0-3
+# There are currently options 0-18
 for e in {0..18}; do
 	echo -e "Currently running ZENO on ${LB}${f}${NC} with expansion option ${ORANGE}${e}${NC}"
 	bash runtests.sh $f $e;
