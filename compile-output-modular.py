76e4b500e71a01de0dff24b9cb92d57483687151
mvk1-nist <mvk1@nist.gov>
2024-06-26 16:30:24 -0400

    Undid previous commit I did this since I both uncommented and modified a large chunk of code in one go. I will make two new commits where those are separate so it would be easier to see the modifications.

--- compile-output-modular.py
+++ compile-output-modular.py
@@ -39,86 +39,84 @@ for filename in os.listdir(dir):
   else:
     everything[g[1]][int(g[0])][0] = csv
 
-# Do the same thing as for the non-modular version, but for each iteration separately
-for iter in range(num_iters):
-  # Process the data structures created in the previous step and
-  # convert them into the contents of the output files
-  result = ("Each list here shows the difference of means between the original output and "
-  "the modified output measured in the original output's standard deviations.\n"
-  "If there is a * symbol next to one of the lists, that means that the standard deviation is less than 0.001 units.\n"
-  "Also, if the standard deviation is 0, it is set to 0.000001 to prevent dividing by 0.\n"
-  "The format for each element is <char><num>:<value> where the chars correspond as follows:\n"
-  "t - total_steps\nh - hit_steps\nm - miss_steps\nc - capacitance\n\n")
-  cols = [f"{c}{i}" for c in ['t', 'h', 'm', 'c'] for i in range(num_mods - 1)]
-  # `df` is used only for the .csv output and `result` is used only for the .txt output
-  # Both are worked on at the same time to prevent duplicate control flows
-  df = pd.DataFrame(columns = cols)
-  for bod in sorted(everything.keys()):
-    df.loc[bod] = [0.0] * ((num_mods - 1) * 4)
-    result += f"{bod}:\n    ["
-    curr = everything[bod][iter]
-    og_df = curr[0]
-    og_total_steps_mean = num_from_df(og_df, "total_steps", "value")
-    og_total_steps_sd = num_from_df(og_df, "total_steps", "std_dev")
-    og_hit_steps_mean = num_from_df(og_df, "hit_steps", "value")
-    og_hit_steps_sd = num_from_df(og_df, "hit_steps", "std_dev")
-    og_miss_steps_mean = num_from_df(og_df, "miss_steps", "value")
-    og_miss_steps_sd = num_from_df(og_df, "miss_steps", "std_dev")
-    og_capac_mean = num_from_df(og_df, "capacitance", "value")
-    og_capac_sd = num_from_df(og_df, "capacitance", "std_dev")
-    og_total_steps_sd = 0.000001 if og_total_steps_sd == 0 else og_total_steps_sd # To prevent a divide by zero error
-    og_hit_steps_sd = 0.000001 if og_hit_steps_sd == 0 else og_hit_steps_sd # To prevent a divide by zero error
-    og_miss_steps_sd = 0.000001 if og_miss_steps_sd == 0 else og_miss_steps_sd # To prevent a divide by zero error
-    og_capac_sd = 0.000001 if og_capac_sd == 0 else og_capac_sd # To prevent a divide by zero error
-    for i in range(1, num_mods):
-      mod_df = curr[i]
-      mod_total_steps_mean = num_from_df(mod_df, "total_steps", "value")
-      mod_total_steps_sd = num_from_df(mod_df, "total_steps", "std_dev")
-      # p-values ended up being unreliable since they were only returning a 1.0 or a 0.0 depending on if the mean was off by a millionth of a unit or not
-      # As a replacement, I will be using the difference between means measured in standard deviations
-      diff = (mod_total_steps_mean - og_total_steps_mean) / og_total_steps_sd
-      result += f"t{i - 1}:{round(diff, 6)}; "
-      averages[0][i - 1] += diff
-      df.loc[bod, f"t{i - 1}"] = round(diff, 6)
-    result += f"\b\b]{' *' if og_total_steps_sd < 0.001 else ''}\tmean:{og_total_steps_mean} sd:{og_total_steps_sd}\n    ["
-    for i in range(1, num_mods):
-      mod_df = curr[i]
-      mod_hit_steps_mean = num_from_df(mod_df, "hit_steps", "value")
-      mod_hit_steps_sd = num_from_df(mod_df, "hit_steps", "std_dev")
-      diff = (mod_hit_steps_mean - og_hit_steps_mean) / og_hit_steps_sd
-      result += f"h{i - 1}:{round(diff, 6)}; "
-      averages[1][i - 1] += diff
-      df.loc[bod, f"h{i - 1}"] = round(diff, 6)
-    result += f"\b\b]{' *' if og_hit_steps_sd < 0.001 else ''}\tmean:{og_hit_steps_mean} sd:{og_hit_steps_sd}\n    ["
-    for i in range(1, num_mods):
-      mod_df = curr[i]
-      mod_miss_steps_mean = num_from_df(mod_df, "miss_steps", "value")
-      mod_miss_steps_sd = num_from_df(mod_df, "miss_steps", "std_dev")
-      diff = (mod_miss_steps_mean - og_miss_steps_mean) / og_miss_steps_sd
-      result += f"m{i - 1}:{round(diff, 6)}; "
-      averages[2][i - 1] += diff
-      df.loc[bod, f"m{i - 1}"] = round(diff, 6)
-    result += f"\b\b]{' *' if og_miss_steps_sd < 0.001 else ''}\tmean:{og_miss_steps_mean} sd:{og_miss_steps_sd}\n    ["
-    for i in range(1, num_mods):
-      mod_df = curr[i]
-      mod_capac_mean = num_from_df(mod_df, "capacitance", "value")
-      mod_capac_sd = num_from_df(mod_df, "capacitance", "std_dev")
-      diff = (mod_capac_mean - og_capac_mean) / og_capac_sd
-      result += f"c{i - 1}:{round(diff, 6)}; "
-      averages[3][i - 1] += diff
-      df.loc[bod, f"c{i - 1}"] = round(diff, 6)
-    result += f"\b\b]{' *' if og_capac_sd < 0.001 else ''}\tmean:{og_capac_mean} sd:{og_capac_sd}"
-    result += "\n\n"
-  result += "AVERAGES:\n"
-  df.loc["AVERAGES"] = [round(e / len(everything.keys()), 6) for lst in averages for e in lst]
-  for sublist in averages:
-    result += f"    ["
-    for e in sublist:
-      e /= len(everything.keys())
-      result += f"{round(e, 6)}; "
-    result += f"\b\b]\n"
+# # Process the data structures created in the previous step and
+# # convert them into the contents of the output files
+# result = ("Each list here shows the difference of means between the original output and "
+# "the modified output measured in the original output's standard deviations.\n"
+# "If there is a * symbol next to one of the lists, that means that the standard deviation is less than 0.001 units.\n"
+# "Also, if the standard deviation is 0, it is set to 0.000001 to prevent dividing by 0.\n"
+# "The format for each element is <char><num>:<value> where the chars correspond as follows:\n"
+# "t - total_steps\nh - hit_steps\nm - miss_steps\nc - capacitance\n\n")
+# cols = [f"{c}{i}" for c in ['t', 'h', 'm', 'c'] for i in range(num_mods - 1)]
+# # `df` is used only for the .csv output and `result` is used only for the .txt output
+# # Both are worked on at the same time to prevent duplicate control flows
+# df = pd.DataFrame(columns = cols)
+# for bod in sorted(everything.keys()):
+#   df.loc[bod] = [0.0] * ((num_mods - 1) * 4)
+#   result += f"{bod}:\n    ["
+#   curr = everything[bod]
+#   og_df = curr[0]
+#   og_total_steps_mean = num_from_df(og_df, "total_steps", "value")
+#   og_total_steps_sd = num_from_df(og_df, "total_steps", "std_dev")
+#   og_hit_steps_mean = num_from_df(og_df, "hit_steps", "value")
+#   og_hit_steps_sd = num_from_df(og_df, "hit_steps", "std_dev")
+#   og_miss_steps_mean = num_from_df(og_df, "miss_steps", "value")
+#   og_miss_steps_sd = num_from_df(og_df, "miss_steps", "std_dev")
+#   og_capac_mean = num_from_df(og_df, "capacitance", "value")
+#   og_capac_sd = num_from_df(og_df, "capacitance", "std_dev")
+#   og_total_steps_sd = 0.000001 if og_total_steps_sd == 0 else og_total_steps_sd # To prevent a divide by zero error
+#   og_hit_steps_sd = 0.000001 if og_hit_steps_sd == 0 else og_hit_steps_sd # To prevent a divide by zero error
+#   og_miss_steps_sd = 0.000001 if og_miss_steps_sd == 0 else og_miss_steps_sd # To prevent a divide by zero error
+#   og_capac_sd = 0.000001 if og_capac_sd == 0 else og_capac_sd # To prevent a divide by zero error
+#   for i in range(1, num_mods):
+#     mod_df = curr[i]
+#     mod_total_steps_mean = num_from_df(mod_df, "total_steps", "value")
+#     mod_total_steps_sd = num_from_df(mod_df, "total_steps", "std_dev")
+#     # p-values ended up being unreliable since they were only returning a 1.0 or a 0.0 depending on if the mean was off by a millionth of a unit or not
+#     # As a replacement, I will be using the difference between means measured in standard deviations
+#     diff = (mod_total_steps_mean - og_total_steps_mean) / og_total_steps_sd
+#     result += f"t{i - 1}:{round(diff, 6)}; "
+#     averages[0][i - 1] += diff
+#     df.loc[bod, f"t{i - 1}"] = round(diff, 6)
+#   result += f"\b\b]{' *' if og_total_steps_sd < 0.001 else ''}\tmean:{og_total_steps_mean} sd:{og_total_steps_sd}\n    ["
+#   for i in range(1, num_mods):
+#     mod_df = curr[i]
+#     mod_hit_steps_mean = num_from_df(mod_df, "hit_steps", "value")
+#     mod_hit_steps_sd = num_from_df(mod_df, "hit_steps", "std_dev")
+#     diff = (mod_hit_steps_mean - og_hit_steps_mean) / og_hit_steps_sd
+#     result += f"h{i - 1}:{round(diff, 6)}; "
+#     averages[1][i - 1] += diff
+#     df.loc[bod, f"h{i - 1}"] = round(diff, 6)
+#   result += f"\b\b]{' *' if og_hit_steps_sd < 0.001 else ''}\tmean:{og_hit_steps_mean} sd:{og_hit_steps_sd}\n    ["
+#   for i in range(1, num_mods):
+#     mod_df = curr[i]
+#     mod_miss_steps_mean = num_from_df(mod_df, "miss_steps", "value")
+#     mod_miss_steps_sd = num_from_df(mod_df, "miss_steps", "std_dev")
+#     diff = (mod_miss_steps_mean - og_miss_steps_mean) / og_miss_steps_sd
+#     result += f"m{i - 1}:{round(diff, 6)}; "
+#     averages[2][i - 1] += diff
+#     df.loc[bod, f"m{i - 1}"] = round(diff, 6)
+#   result += f"\b\b]{' *' if og_miss_steps_sd < 0.001 else ''}\tmean:{og_miss_steps_mean} sd:{og_miss_steps_sd}\n    ["
+#   for i in range(1, num_mods):
+#     mod_df = curr[i]
+#     mod_capac_mean = num_from_df(mod_df, "capacitance", "value")
+#     mod_capac_sd = num_from_df(mod_df, "capacitance", "std_dev")
+#     diff = (mod_capac_mean - og_capac_mean) / og_capac_sd
+#     result += f"c{i - 1}:{round(diff, 6)}; "
+#     averages[3][i - 1] += diff
+#     df.loc[bod, f"c{i - 1}"] = round(diff, 6)
+#   result += f"\b\b]{' *' if og_capac_sd < 0.001 else ''}\tmean:{og_capac_mean} sd:{og_capac_sd}"
+#   result += "\n\n"
+# result += "AVERAGES:\n"
+# df.loc["AVERAGES"] = [round(e / len(everything.keys()), 6) for lst in averages for e in lst]
+# for sublist in averages:
+#   result += f"    ["
+#   for e in sublist:
+#     e /= len(everything.keys())
+#     result += f"{round(e, 6)}; "
+#   result += f"\b\b]\n"
 
-  # Write output into the output files
-  with open(f"outs/out-{iter}.txt", "w") as f:
-    f.write(result)
-  df.to_csv(f"outs/out-{iter}.csv")
+# # Write output into the output files
+# with open("out-modular.txt", "w") as f:
+#   f.write(result)
+# df.to_csv("out-modular.csv")
