e8ea522190aa68e41e8b2a06cfd349123026bcb0
mvk1-nist <mvk1@nist.gov>
2024-07-25 17:58:59 -0400

    The graphs no longer use standard deviations as a unit Instead, they use the native units as outputted by ZENO. Also a few other changes.

--- compile-output-modular.py
+++ compile-output-modular.py
@@ -44,10 +44,10 @@ for filename in os.listdir(dir):
     everything[g[1]][int(g[0])][0] = csv
 
 # Do the same thing as for the non-modular version, but for each iteration separately
+cols = [f"{c}{i}" for c in ['t', 'h', 'm', 'c'] for i in range(num_mods - 1)]
 for iter in range(num_iters):
   # Process the data structures created in the previous step and
   # convert them into the contents of the output files
-  cols = [f"{c}{i}" for c in ['t', 'h', 'm', 'c'] for i in range(num_mods - 1)]
   df = pd.DataFrame(columns = cols)
   for bod in sorted(everything.keys()):
     averages = [[0] * (num_mods - 1) for _ in range(4)]
@@ -55,45 +55,31 @@ for iter in range(num_iters):
     curr = everything[bod][iter]
     og_df = curr[0]
     og_total_steps_mean = num_from_df(og_df, "total_steps", "value")
-    og_total_steps_sd = num_from_df(og_df, "total_steps", "std_dev")
     og_hit_steps_mean = num_from_df(og_df, "hit_steps", "value")
-    og_hit_steps_sd = num_from_df(og_df, "hit_steps", "std_dev")
     og_miss_steps_mean = num_from_df(og_df, "miss_steps", "value")
-    og_miss_steps_sd = num_from_df(og_df, "miss_steps", "std_dev")
     og_capac_mean = num_from_df(og_df, "capacitance", "value")
-    og_capac_sd = num_from_df(og_df, "capacitance", "std_dev")
-    og_total_steps_sd = 0.000001 if og_total_steps_sd == 0 else og_total_steps_sd # To prevent a divide by zero error
-    og_hit_steps_sd = 0.000001 if og_hit_steps_sd == 0 else og_hit_steps_sd # To prevent a divide by zero error
-    og_miss_steps_sd = 0.000001 if og_miss_steps_sd == 0 else og_miss_steps_sd # To prevent a divide by zero error
-    og_capac_sd = 0.000001 if og_capac_sd == 0 else og_capac_sd # To prevent a divide by zero error
     for i in range(1, num_mods):
       mod_df = curr[i]
       mod_total_steps_mean = num_from_df(mod_df, "total_steps", "value")
-      mod_total_steps_sd = num_from_df(mod_df, "total_steps", "std_dev")
-      # p-values ended up being unreliable since they were only returning a 1.0 or a 0.0 depending on if the mean was off by a millionth of a unit or not
-      # As a replacement, I will be using the difference between means measured in standard deviations
-      diff = (mod_total_steps_mean - og_total_steps_mean) / og_total_steps_sd
+      diff = (mod_total_steps_mean - og_total_steps_mean)
       averages[0][i - 1] += diff
       df.loc[bod, f"t{i - 1}"] = round(diff, 6)
     for i in range(1, num_mods):
       mod_df = curr[i]
       mod_hit_steps_mean = num_from_df(mod_df, "hit_steps", "value")
-      mod_hit_steps_sd = num_from_df(mod_df, "hit_steps", "std_dev")
-      diff = (mod_hit_steps_mean - og_hit_steps_mean) / og_hit_steps_sd
+      diff = (mod_hit_steps_mean - og_hit_steps_mean)
       averages[1][i - 1] += diff
       df.loc[bod, f"h{i - 1}"] = round(diff, 6)
     for i in range(1, num_mods):
       mod_df = curr[i]
       mod_miss_steps_mean = num_from_df(mod_df, "miss_steps", "value")
-      mod_miss_steps_sd = num_from_df(mod_df, "miss_steps", "std_dev")
-      diff = (mod_miss_steps_mean - og_miss_steps_mean) / og_miss_steps_sd
+      diff = (mod_miss_steps_mean - og_miss_steps_mean)
       averages[2][i - 1] += diff
       df.loc[bod, f"m{i - 1}"] = round(diff, 6)
     for i in range(1, num_mods):
       mod_df = curr[i]
       mod_capac_mean = num_from_df(mod_df, "capacitance", "value")
-      mod_capac_sd = num_from_df(mod_df, "capacitance", "std_dev")
-      diff = (mod_capac_mean - og_capac_mean) / og_capac_sd
+      diff = (mod_capac_mean - og_capac_mean)
       diff = abs(diff) if interior_abs else diff
       averages[3][i - 1] += diff
       df.loc[bod, f"c{i - 1}"] = round(diff, 6)
@@ -113,26 +99,26 @@ final_dfs = list(range(num_iters)) # List of dataframes
 for i in range(num_iters):
   final_dfs[i] = pd.read_csv(f"{outs}/out-{i}.csv").iloc[-1:, 1:] # So that I only have the numbers on the row AVERAGES
 
-result = None
+result = pd.DataFrame(0.0, index = ["mean", "std_dev"], columns = cols)
+n = 1
 for df in final_dfs:
-  if type(result) == type(None):
-    result = df
-  else:
-    result += df
+  if n > 1:
+    result.loc["std_dev"] += ((((df.iloc[0] - result.loc["mean"]) ** 2) / n) - (result.loc["std_dev"] / (n - 1)))
+  result.loc["mean"] += ((df.iloc[0] - result.loc["mean"]) / n)
+  n += 1
 
-result /= num_iters
+result.loc["std_dev"] = result.loc["std_dev"] ** 0.5
 
 result.to_csv("out-modular.csv")
-# This redundancy is done so that I can make sure the csv is saved properly during debugging. The redundancy may be removed later.
-data = pd.read_csv("out-modular.csv")
+data = result # Leftover stuff, may fix in a later update
 
 # Graph the total step differences and the absolute capacitance differences
 nums = list(range(num_mods - 1))
-steps = data.iloc[:,1:num_mods].values.tolist()[0]
-capac = [abs(i) for i in data.iloc[:,(-num_mods + 1):].values.tolist()[0]]
+steps = data.iloc[0,0:(num_mods - 1)].values.tolist()
+steps_sd = data.iloc[1,0:(num_mods - 1)].values.tolist()
+capac = [abs(i) for i in data.iloc[0,(-num_mods + 1):].values.tolist()]
+capac_sd = [abs(i) for i in data.iloc[1,(-num_mods + 1):].values.tolist()]
 colors = ['red'] * 1 + ['orange'] * 5 + ['yellow'] * 3 + ['green'] * 9 + ['blue'] * 1
-
-cap = 2.0 * (2.0 / math.sqrt(math.pi)) if interior_abs else 10.0 * capac[0]
 
 red_patch = mpatches.Patch(color='red', label='Control')
 orange_patch = mpatches.Patch(color='orange', label='Constant >=e')
@@ -140,57 +126,63 @@ yellow_patch = mpatches.Patch(color='yellow', label='Constant <e')
 green_patch = mpatches.Patch(color='green', label='Proportional')
 blue_patch = mpatches.Patch(color='blue', label='Random')
 
+plt.figure(figsize=(9,6))
 plt.bar(nums, steps, align='center', alpha=0.5, color=colors)
 plt.xticks(nums, nums)
 plt.xlabel('Method of Expansion Number')
-plt.ylabel('Difference in standard deviations')
-plt.errorbar(nums, steps, yerr = 1, fmt ='_', color = 'black')
+plt.ylabel('Difference')
+plt.errorbar(nums, steps, yerr = steps_sd, fmt ='_', color = 'black')
 plt.title('Total Steps')
 plt.legend(handles=[red_patch, orange_patch, yellow_patch, green_patch, blue_patch])
 plt.savefig("steps_large_modular.png")
 
 plt.clf()
+plt.figure(figsize=(9,6))
 plt.bar(nums, capac, align='center', alpha=0.5, color=colors)
 plt.xticks(nums, nums)
 plt.xlabel('Method of Expansion Number')
 if interior_abs:
-  plt.ylabel('|Absolute difference in standard deviations|')
+  plt.ylabel('|Absolute difference|')
 else:
-  plt.ylabel('|Difference in standard deviations|')
-plt.errorbar(nums, capac, yerr = 1, fmt ='_', color = 'black')
+  plt.ylabel('|Difference|')
+plt.errorbar(nums, capac, yerr = capac_sd, fmt ='_', color = 'black')
 plt.title('Capacitance')
 plt.legend(handles=[red_patch, orange_patch, yellow_patch, green_patch, blue_patch])
-plt.axhline(y = cap, color='r', linestyle='-')
+plt.axhline(y = capac[0] + capac_sd[0], color='r', linestyle=':')
 plt.savefig("capac_large_modular.png")
 
-# Create a boolean list that represents the indices of expansion options with a capacitance difference less than cap
-remainders = [True if i < cap else False for i in capac]
+# Create a boolean list that represents the indices of expansion options we want to keep
+c_m = capac[0] # control mean
+c_s = capac_sd[0] # control sd
+remainders = [True if (c_m + c_s > m - s) else False for (m, s) in zip(capac, capac_sd)]
 remainder_steps = [i for i, r in zip(steps, remainders) if r]
+remainder_steps_sd = [i for i, r in zip(steps_sd, remainders) if r]
 remainder_capac = [i for i, r in zip(capac, remainders) if r]
+remainder_capac_sd = [i for i, r in zip(capac_sd, remainders) if r]
 remainder_colors = [i for i, r in zip(colors, remainders) if r]
 remainder_nums = [i for i, r in zip(nums, remainders) if r]
 remainder_range = list(range(len(remainder_nums)))
 
-# Graph the same graphs as before, but only include the expansion options for which the capacitance is less than cap
+# Graph the same graphs as before, but only include the expansion options that we want to keep
 plt.clf()
+plt.figure(figsize=(9,6))
 plt.bar(remainder_range, remainder_steps, align='center', alpha=0.5, color=remainder_colors)
 plt.xticks(remainder_range, remainder_nums)
 plt.xlabel('Method of Expansion Number')
-plt.ylabel('Difference in standard deviations')
+plt.ylabel('Difference')
 plt.title('Total Steps')
 plt.legend(handles=[red_patch, orange_patch, yellow_patch, green_patch, blue_patch])
 plt.savefig("steps_small_modular.png")
 
 plt.clf()
+plt.figure(figsize=(9,6))
 plt.bar(remainder_range, remainder_capac, align='center', alpha=0.5, color=remainder_colors)
 plt.xticks(remainder_range, remainder_nums)
 plt.xlabel('Method of Expansion Number')
 if interior_abs:
-  plt.ylabel('|Absolute difference in standard deviations|')
+  plt.ylabel('|Absolute difference|')
 else:
-  plt.ylabel('|Difference in standard deviations|')
+  plt.ylabel('|Difference|')
 plt.title('Capacitance')
 plt.legend(handles=[red_patch, orange_patch, yellow_patch, green_patch, blue_patch])
-if interior_abs:
-  plt.axhline(y = 2 / math.sqrt(math.pi), color='b', linestyle='-')
 plt.savefig("capac_small_modular.png")
